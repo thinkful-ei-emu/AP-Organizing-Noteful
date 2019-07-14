a87b441a90bbe5ed824a09b17e09d25534b175cb
Arpan Patel <adp8ke@gmail.com>
2019-07-14 17:38:43 -0400

    added basic css to add folder component

--- src/components/addfolder.js
+++ src/components/addfolder.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import StoreContext from '../context/StoreContext'
-
+import './styles/addfolder.css';
 
 class AddFolder extends React.Component {
 
@@ -10,13 +10,14 @@ class AddFolder extends React.Component {
 
         return(
             <form onSubmit={e => this.context.newFolder(e)}>
-                <label>New Folder Name:</label>
+                <label htmlFor="new-folder" className="new-folder-label">New Folder Name:</label>
                 <input
                     type="text" 
                     name="newFolder"
                     placeholder="New Folder"
                     onChange={e => this.context.newFolderName(e.target.value)}
                     required
+                    className="new-folder-input"
                 />
                 <button type="submit">Add New Folder</button>
             </form>
